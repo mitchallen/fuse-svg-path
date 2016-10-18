8acec8ba294a083fd543d54104310779a8d80231
Mitch Allen <mitch.c.allen@gmail.com>
2016-10-18 09:56:02 -0700

    added test for fusing reversed paths with middle ops

--- test/fuse-test.js
+++ test/fuse-test.js
@@ -86,7 +86,7 @@ describe('fuse method', function() {
         var obj = _factory.create({});
         should.exist(obj);
         let options = {
-            verbose: true,
+            verbose: false,
             path: [
                 { op: "M", x: 10, y: 20 },
                 { op: "L", x: 15, y:  5 },
@@ -94,6 +94,33 @@ describe('fuse method', function() {
                 { op: "M", x: 30, y: 40 },
                 { op: "L", x: 50, y: 55 },
                 { op: "L", x: 25, y: 35 }
+            ]
+        };
+        let expected = [
+            { op: "M", x: 10, y: 20 },
+            { op: "L", x: 15, y:  5 },
+            { op: "L", x: 30, y: 40 },
+            { op: "L", x: 50, y: 55 },
+            { op: "L", x: 25, y: 35 }
+        ];
+        var result = obj.fuse(options);
+        should.exist(result);
+        result.should.eql(expected);
+        done();
+    });
+
+    it('should fuse reversed paths with middle ops', function(done) {
+        var obj = _factory.create({});
+        should.exist(obj);
+        let options = {
+            verbose: false,
+            path: [
+                { op: "M", x: 10, y: 20 },
+                { op: "L", x: 15, y:  5 },
+                { op: "L", x: 30, y: 40 },
+                { op: "M", x: 25, y: 35 },
+                { op: "L", x: 50, y: 55 },
+                { op: "L", x: 30, y: 40 }
             ]
         };
         let expected = [
